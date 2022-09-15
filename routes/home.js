808a70554aa14408bb07252837752c566e8a4aa2
Abhiram <abhiram@zdistancelab.com>
2022-09-15 16:43:21 +0530

    test

--- routes/home.js
+++ routes/home.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const {
-    removeBook,
-    viewBook,
-    updateBook,
-    homeDummy,
-    getBookList,
-    addBookList,
-    issueBook,
-    returnBook,
-    getIssueListt
-} = require("../controller/homeController");
-const {isLoggedIn, customRole} = require("../middlewares/user");
+ const express = require("express");
+// const {
+//     removeBook,
+//     viewBook,
+//     updateBook,
+//     homeDummy,
+//     getBookList,
+//     addBookList,
+//     issueBook,
+//     returnBook,
+//     getIssueListt
+// } = require("../controller/homeController");
+// const {isLoggedIn, customRole} = require("../middlewares/user");
 
 const router = express.Router();
 
 router.route("/").get(homeDummy);
-router.route("/userdashboard").get(isLoggedIn, getBookList);
+// router.route("/userdashboard").get(isLoggedIn, getBookList);
 
-router.route("/admin/addbooks").post(isLoggedIn, customRole("admin"), addBookList);
-router.route("/admin/book/:id")
-.get(isLoggedIn, customRole("admin"), viewBook).put(isLoggedIn, customRole("admin"), updateBook).delete(isLoggedIn, customRole("admin"), removeBook)
+// router.route("/admin/addbooks").post(isLoggedIn, customRole("admin"), addBookList);
+// router.route("/admin/book/:id")
+// .get(isLoggedIn, customRole("admin"), viewBook).put(isLoggedIn, customRole("admin"), updateBook).delete(isLoggedIn, customRole("admin"), removeBook)
 
-router.route("/admin/issueBook").post(isLoggedIn, customRole("admin"), issueBook);
-router.route("/admin/returnBook/:id").post(isLoggedIn, customRole("admin"), returnBook);
-router.route("/admin/issueList").get(isLoggedIn, customRole("admin"), getIssueListt);
+// router.route("/admin/issueBook").post(isLoggedIn, customRole("admin"), issueBook);
+// router.route("/admin/returnBook/:id").post(isLoggedIn, customRole("admin"), returnBook);
+// router.route("/admin/issueList").get(isLoggedIn, customRole("admin"), getIssueListt);
 
-module.exports = router;
+ module.exports = router;
