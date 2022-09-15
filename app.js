77f861d1a91994a7551815f15e3e54de63766faa
Abhiram <abhiram@zdistancelab.com>
2022-09-15 16:30:23 +0530

    test

--- app.js
+++ app.js
@@ -10,22 +10,23 @@ const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// regular middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// cookies and file middleware
-app.use(cookieParser());
-
-app.use(morgan("tiny"));
-
 app.get("/", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
-// import all routes here
-const home = require("./routes/home");
-const user = require("./routes/user");
-// router middleware
-app.use("/api/v1", home);
-app.use("/api/v1", user);
+// // regular middleware
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+// // cookies and file middleware
+// app.use(cookieParser());
+
+// app.use(morgan("tiny"));
+
+
+// // import all routes here
+// const home = require("./routes/home");
+// const user = require("./routes/user");
+// // router middleware
+// app.use("/api/v1", home);
+// app.use("/api/v1", user);
 module.exports = app;
