838dd98b23a13e154b70e0b908387b43915ceb39
Abhiram <abhiram@zdistancelab.com>
2022-09-15 17:37:36 +0530

    test

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,4 +1,4 @@
-const BigPromise = require("../middlewares/bigPromise");
+//const BigPromise = require("../middlewares/bigPromise");
 const Bookdata = require("../models/book");
 const Issuedata = require("../models/issue");
 const Recorddata = require("../models/record");
