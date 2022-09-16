39beec3151d1eee669abea4f7e2fd2496fa05b25
Abhiram <abhiram@zdistancelab.com>
2022-09-16 11:50:02 +0530

    test

--- middlewares/bigpromise.js
+++ middlewares/bigpromise.js
@@ -1,2 +1,5 @@
-module.exports = (func) => (req, res, next) =>
-  Promise.resolve(func(req, res, next)).catch(next);
+module.exports = (func) => (req, res, next) => Promise.resolve(func(req, res, next)).then((value) => {
+    next
+}, (reason) => {
+    res.status(400).json({success: true, greeting: "API Failed", reason: reason});
+},);
