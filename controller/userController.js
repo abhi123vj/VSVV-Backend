de2aeacf8bebee5978ca991d1d966cf9c96ec986
Abhiram <abhiram@zdistancelab.com>
2022-09-16 12:15:08 +0530

    tesdt

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,9 @@
-const User = require("../models/users");
-const BigPromise = require("../middlewares/bigPromise");
-const CustomError = require("../utils/customErrors");
-const cookieToken = require("../utils/cookieToken");
+// const User = require("../models/users");
+// const BigPromise = require("../middlewares/bigPromise");
+// const CustomError = require("../utils/customErrors");
+// const cookieToken = require("../utils/cookieToken");
 
-// exports.signup = BigPromise(async (req, res, next) => {
+// // exports.signup = BigPromise(async (req, res, next) => {
 //   //let result;
 
 //   const { name, email, password,role } = req.body;
@@ -94,40 +94,3 @@ const cookieToken = require("../utils/cookieToken");
 //   cookieToken(user, res);
 // });
 
-
-exports.login = async  (req, res, next) => {
-  try {const { email, password } = req.body;
-  // check for presence of email and password
-  if (!email || !password) {
-    return next(new CustomError("please provide email and password", 400));
-  }
-
-  // get user from DB
-  const user = await User.findOne({ email }).select("+password");
-
-
-  // if user not found in DB
-  if (!user) {
-    return next(
-      new CustomError("Email or password does not match or exist", 400)
-    );
-  }
-
-  // match the password
-  const isPasswordCorrect = await user.isValidatedPassword(password);
-
-  //if password do not match
-  if (!isPasswordCorrect) {
-    return next(
-      new CustomError("Email or password does not match or exist", 400)
-    );
-  }
-
-  // if all goes good and we send the token
-  cookieToken(user, res);
-}catch(e){
-  res.status(400).json({
-    success: false,
- error:e
-  });
-}}
