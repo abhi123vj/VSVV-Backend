752250f596888631d75aa2d283debd117dbdb54f
Abhiram S <4bh1ram@gmail.com>
2022-09-18 14:23:49 +0530

    make it better

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require("express");
-
+var cors = require('cors')
 require("dotenv").config();
 const app = express();
 var morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+app.use(cors())
 // for swagger documentation
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
