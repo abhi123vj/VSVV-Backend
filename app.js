e8b12563c97b0f33a947069dfef8da8179265f1a
Abhiram S <4bh1ram@gmail.com>
2022-09-18 14:12:24 +0530

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
+
 require("dotenv").config();
 const app = express();
 var morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
 // for swagger documentation
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const CustomError = require("./utils/customErrors");
 
 // regular middleware
 app.use(express.json());
@@ -25,4 +39,18 @@ const user = require("./routes/user");
 // router middleware
 app.use("/api/v1", home);
 app.use("/api/v1", user);
+
+app.use((req, res, next) => {
+  next(next(new CustomError("Not Found", 404)));
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({
+    success: false,
+    errorMessage:  error.message,
+
+  });
+});
+
 module.exports = app;
--- middlewares/bigpromise.js
+++ middlewares/bigpromise.js
@@ -1,4 +1,18 @@
 // try catch and async - await || use promise
+const CustomError = require("../utils/customErrors");
 
 module.exports = (func) => (req, res, next) =>
-  Promise.resolve(func(req, res, next)).catch(next);
+  Promise.resolve(func(req, res, next))
+    .then(
+      (value) => {
+        console.log("sucess");
+
+        console.log(value); // "Success"
+      },
+      (reason) => {
+        if (reason.code == 11000)
+          next(new CustomError("User Already exsist", 406 ));
+        else next(new CustomError("Internal Server Error", 500));
+      }
+    )
+    .catch(next);
